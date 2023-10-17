485e8ebceb08ceaad5de8e97b45a897748f99b06
Ataf_Georges <ataf_eleid@hotmail.com>
2023-10-17 16:57:05 +0200

    modification de JS en train de faire mes btn sur l'ecran

---
+++ assests/script/script.js
@@ -0,0 +1,98 @@
+//------------crée mon tableau de alpha lists-----------
+let alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+//--------Crée mes paramètres ------------
+const devineMots = ['dragon', 'flemme', 'bleu', 'coder', 'manger', 'jouer', 'dormir', 'monstre', 'console', 'boire', 'baleine', 'chat', 'dauphin',
+  'huile', 'angle', 'armoire', 'banc', 'bureau', 'cabinet', 'carreau', 'chaise', 'classe', 'coin', 'couloir', 'dossier'];
+let mot;
+let devine = [];
+let devineStocke = "dauphin";
+const arrayMotMystere = devineStocke.split('')
+console.log(arrayMotMystere);
+let laVie;
+let compteVraiDevine;
+let espace;
+let porteMots;
+//------------------crée mon alératoire----------
+function porterMonAleratoire() {
+  return  motAleratoire = Math.floor(Math.random() * devineMots.length);
+}
+
+console.log(motAleratoire);
+/// ----------crée en cadren 'affichage------
+let main = document.createElement("main");
+document.body.appendChild(main);
+let imgpendu = document.createElement("image");
+imgpendu.classList.add("img-pendu");
+imgpendu.src = "../img/pendu1.JPG"
+main.appendChild(imgpendu);
+//-----------crée mon motSécret--------------
+let motSecret = document.createElement("div");
+motSecret.classList.add("mot-secret");
+main.appendChild(motSecret);
+
+//-------------crée container---------------
+let container = document.createElement("div");
+container.classList.add("container");
+main.appendChild(container);
+//-------------- crée l'afficheur mots-------
+let afficheMots = document.createElement("div");
+afficheMots.classList.add = "affiche-mots";
+container.appendChild(afficheMots);
+//--------------crée alpha buttons-------------
+for (let index = 0; index < alphabet.length; index++) {
+  let alphaBtn = document.createElement("button");
+  alphaBtn.innerText = alphabet[index];
+  alphaBtn.classList.add("alpha-btn");
+  alphaBtn.setAttribute('id', "mon_mots");
+  container.appendChild(alphaBtn);
+}
+//-------------crée mon function pour gérrer mes buttons   ---------
+let examiner = document.querySelectorAll(".alpha-btn");
+examiner.forEach((unBtn, index) => {
+  unBtn.addEventListener("click", (eventClick) => {
+    console.log("Index : ", index + " " + unBtn);
+    if (devine.includes(unBtn.innerText)) {
+      console.log("Vous avez déjà trouvé cette lettre.");
+    } else {
+      devine.push(unBtn.innerText);
+      if (arrayMotMystere.includes(unBtn.innerText)) {
+        console.log("il est examiner -c'est  OK");
+        console.log(unBtn.innerText);
+      } else {
+        console.log("c'est n'ai pas convient de mon tableau ");
+      }
+    }
+    console.log(devine);
+  })
+})
+
+
+
