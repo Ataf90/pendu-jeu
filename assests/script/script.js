e1f87869214dc2bf795cf3dd4df32084a429ee19
Ataf_Georges <ataf_eleid@hotmail.com>
2023-10-21 14:35:02 +0200

    le jeu pendu présque finit

--- assests/script/script.js
+++ assests/script/script.js
@@ -1,119 +1,237 @@
-//------------crée mon tableau de alpha lists-----------
-let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+console.log("script chargé ma gueule");
+
+// -------------------------Generateur de mots --------------------
+let mots = [
+  "jacuzzi",
+  "manches",
+  "jukebox",
+  "bazooka",
+  "clavier",
+  "ballons",
+  "biscuit",
+  "aquagym",
+  "cubitus",
+  "estomac",
+  "algebre",
+  "racines",
+  "trousse",
+  "homards",
+  "neutron",
+  "hache",
+  "anticonstitutionnel",
+  "rubis",
+  "palindrome",
+  "carburateur",
 ];
 
-
-//--------Crée mes paramètres ------------
-const devineMots = ['dragon', 'flemme', 'bleu', 'coder', 'manger', 'jouer', 'dormir', 'monstre', 'console', 'boire', 'baleine', 'chat', 'dauphin',
-  'huile', 'angle', 'armoire', 'banc', 'bureau', 'cabinet', 'carreau', 'chaise', 'classe', 'coin', 'couloir', 'dossier'];
-let saisie = [];
-let devineStocke = "";
-const arrayMotMystere = devineStocke.split('');
-
-// let laVie;
-// let compteVraiDevine;
-let motAleatoire = devineMots[Math.floor(Math.random() * devineMots.length)]; /// crée mot aléatoire
-
-let espace = "_";
-// let trouveMot = [];
-// for (let i = 0; i < motAleatoire.length; i++) {
-//   trouveMot[i] = "_";
-//}
-let nombreDevie = 6;
-let nombreRestant = motAleatoire.length;
-const pendu = ["/assests/img/pendu2.png", "/assests/img/pendu3.png", "/assests/img/pendu4.png", "/assests/img/pendu5.png", "/assests/img/pendu6.png", "/assests/img/pendu7.png"];
-let indexImage = 1;
-//------------------crée mon alératoire----------
-
+let motAleatoire = mots[Math.floor(Math.random() * mots.length)];
+motAleatoire = motAleatoire.toUpperCase();
+const arrayMotAleatoire = motAleatoire.split("");
+console.log(arrayMotAleatoire);
+let lettresRestantes = motAleatoire.length;
 console.log(motAleatoire);
-/// ----------crée en cadren 'affichage Main------
-let main = document.createElement("main");
-document.body.appendChild(main);
-//--------------crée mon imgae-------------
+
+// ----------------------------------------------------------------------
+
+let lettresClavier = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+let saisie = [];
+let trouveMot = [];
+for (let i = 0; i < motAleatoire.length; i++) {
+  trouveMot[i] = "_";
+}
+let nombreDeVies = 5;
+let trouve = false;
+const pendu = [
+  "/assests/img/deux.png",
+  "./assets/img/trois.png",
+  "./assets/img/quatre.png",
+  "./assets/img/cinq.png",
+  "./assets/img/six.png",
+  "./assets/img/sept.png",
+];
+
+let indexImage = 0;
+// -------------------Clavier ---------------------------------------------
+
+let main = document.getElementById("main");
+let cadreImage = document.getElementById("pendu");
+
+document.getElementById("mot_a_trouver").innerHTML = trouveMot.join(" ");
+
 let image = document.createElement("img");
-image.setAttribute("id", "img_pendu")
-image.setAttribute("src", "/assests/img/pendu1.png");
-main.appendChild(image);
-//-----------crée mon motSécret--------------
-let motSecret = document.createElement("div");
-motSecret.classList.add("mot-secret");
-// motSecret.innerHTML = trouveMot.join(" ");
-main.appendChild(motSecret);
-//----------- crée  Resultat-------------------
+image.setAttribute("id", "img_pendu");
+image.setAttribute("src", "/assests/img/un.png");
+cadreImage.appendChild(image);
+
 let cadreResultat = document.getElementById("resultat");
-//-----------crée une celule-----------------
-for (let index = 0; index < motAleatoire.length; index++) {
-  let celule = document.createElement("div");
-  celule.classList.add("celule-devine");
-  motSecret.appendChild(celule);
+
+let image2 = document.createElement("img");
+image2.setAttribute("src", "");
+image2.setAttribute("id", "image_resultat");
+cadreResultat.appendChild(image2);
+
+let cadreClavier = document.getElementById("cadre_clavier");
+let clavier = document.createElement("div");
+clavier.classList.add("clavier");
+cadreClavier.appendChild(clavier);
+
+for (let index = 0; index < 26; index++) {
+  let bouton = document.createElement("button");
+  bouton.innerText = lettresClavier[index];
+  bouton.classList.add("bouton");
+  bouton.setAttribute("id", index);
+  clavier.appendChild(bouton);
 }
+let boutonRefresh = document.createElement("button");
+boutonRefresh.setAttribute("id", "bouton_refresh");
+boutonRefresh.innerText = "REJOUER";
+boutonRefresh.addEventListener("click", () => {
+  location.reload();
+});
+// --------------------- Jeu ---------------------------------------
 
-// for (let index = 0; index < arrayMotMystere.length; index++) {
-//   celule.innerText += arrayMotMystere[index] + '_';
-// }
-
-//-------------crée container---------------
-let container = document.createElement("div");
-container.classList.add("container");
-main.appendChild(container);
-//-------------- crée l'afficheur mots-------
-let afficheMots = document.createElement("div");
-afficheMots.classList.add = "affiche-mots";
-container.appendChild(afficheMots);
-//--------------crée alpha buttons-------------
-for (let index = 0; index < alphabet.length; index++) {
-  let alphaBtn = document.createElement("button");
-  alphaBtn.innerText = alphabet[index];
-  alphaBtn.classList.add("alpha-btn");
-  alphaBtn.setAttribute('id', "mon_mots");
-  container.appendChild(alphaBtn);
-}
-//-------------crée mon function pour gérrer mes buttons   ---------
-let examiner = document.querySelectorAll(".alpha-btn");
-examiner.forEach((unBtn, indexBtn) => { /// unBtn =>>> container de mon evenment de chaque click de button
-  unBtn.addEventListener("click", (eventClick) => {
-    console.log("Index : ", indexBtn + " " + unBtn);
-    if (saisie.includes(unBtn.innerText)) {
-      console.log("Vous avez déjà trouvé cette lettre.");
-    } else {
-      saisie.push(unBtn.innerText);
-      if (motAleatoire.includes(unBtn.innerText)) {
-        console.log("il est examiner -c'est  OK");
-        unBtn.style.backgroundColor = "gray";
-        unBtn.setAttribute("disabled", "");
-        nombreRestant--;
-
-        // console.log("index :", indexTiret);
-        // console.log("unTiret :", unTiret);
-        if (nombreRestant == 0) {
-          console.log("lettres restantes = " + nombreRestant);
-          console.log("vous avez gagné !");
-        }
-        document.querySelectorAll(".mot-secret > div").forEach((unTiret, indexTiret) => {
-          for (let indexMotMystere = 0; indexMotMystere < motAleatoire.length; indexMotMystere++) {
-            if (motAleatoire[indexMotMystere] === unBtn.innerText && indexMotMystere == indexTiret) {
-              // console.log("arrayMotMystere[indexMotMystere] :", motAleatoire[indexMotMystere]);
-              // console.log("unBtn.innerText :", unBtn.innerText);
-
-              unTiret.innerText =  motAleatoire[indexMotMystere];
-            }
-          }
-          console.log(unBtn.innerText);
-        })
-      } else {
-        console.log("c'est n'ai pas bon ");
-        indexImage++;
-        image.setAttribute("src", `./assests/img/pendu${indexImage}.png`)
-        nombreDevie--;
-        console.log("nombre de vies restantes =   " + nombreDevie);
-        if (nombreDevie === 0) {
-          console.log("vous avez perdu !");
+let essai = document.querySelectorAll(".bouton");
+essai.forEach((unBouton) => {
+  unBouton.addEventListener("click", (eventClick) => {
+    let ecoute = eventClick.target.innerText;
+    unBouton.setAttribute("disabled", "");
+    for (let j = 0; j < motAleatoire.length; j++) {
+      if (motAleatoire[j].includes(ecoute)) {
+        lettresRestantes--;
+        saisie.push(ecoute);
+        console.log("saisie = " + saisie);
+        trouve = true;
+        trouveMot[j] = ecoute;
+        if (lettresRestantes == 0) {
+          image2.setAttribute("src", "/assests/img/gagner.jpg");
+          console.log("C'EST GAGNÉ");
+          clavier.style.display = "none";
+          cadreResultat.appendChild(boutonRefresh);
+          break;
         }
       }
+      console.log("lettres restantes = " + lettresRestantes);
     }
-    console.log(saisie);
-  })
-})
+    if (trouve === false) {
+      nombreDeVies--;
+      indexImage++;
+      image.setAttribute("src", pendu[indexImage]);
+    }
 
+    console.log(nombreDeVies);
+    console.log("nombre de vies = " + nombreDeVies);
 
+    if (nombreDeVies == 0) {
+      resultat = "PERDU";
+      console.log("PERDU");
+      image2.setAttribute("src", "/assests/img/perdu.png");
+      clavier.style.display = "none";
+      cadreResultat.appendChild(boutonRefresh);
+    }
+    trouve = false;
+    console.log(trouveMot);
+    document.getElementById("mot_a_trouver").innerHTML = trouveMot.join(" ");
+  });
+});
 
+//-----------crée une celule-----------------
+// for (let index = 0; index < motAleatoire.length; index++) {
+//   let celule = document.createElement("div");
+//   celule.classList.add("celule-devine");
+//   motSecret.appendChild(celule);
+// }
+
+// // for (let index = 0; index < arrayMotMystere.length; index++) {
+// //   celule.innerText += arrayMotMystere[index] + '_';
+// // }
+
+// //-------------crée container---------------
+// let container = document.createElement("div");
+// container.classList.add("container");
+// main.appendChild(container);
+// //-------------- crée l'afficheur mots-------
+// let afficheMots = document.createElement("div");
+// afficheMots.classList.add = "affiche-mots";
+// container.appendChild(afficheMots);
+// //--------------crée alpha buttons-------------
+// for (let index = 0; index < alphabet.length; index++) {
+//   let alphaBtn = document.createElement("button");
+//   alphaBtn.innerText = alphabet[index];
+//   alphaBtn.classList.add("alpha-btn");
+//   alphaBtn.setAttribute('id', "mon_mots");
+//   container.appendChild(alphaBtn);
+// }
+// //-------------crée mon function pour gérrer mes buttons   ---------
+// let examiner = document.querySelectorAll(".alpha-btn");
+// examiner.forEach((unBtn, indexBtn) => { /// unBtn =>>> container de mon evenment de chaque click de button
+//   unBtn.addEventListener("click", (eventClick) => {
+//     console.log("Index : ", indexBtn + " " + unBtn);
+//     if (saisie.includes(unBtn.innerText)) {
+//       console.log("Vous avez déjà trouvé cette lettre.");
+//     } else {
+//       saisie.push(unBtn.innerText);
+//       if (motAleatoire.includes(unBtn.innerText)) {
+//         console.log("il est examiner -c'est  OK");
+//         unBtn.style.backgroundColor = "gray";
+//         unBtn.setAttribute("disabled", "");
+//         nombreRestant--;
+
+//         // console.log("index :", indexTiret);
+//         // console.log("unTiret :", unTiret);
+//         if (nombreRestant == 0) {
+//           console.log("lettres restantes = " + nombreRestant);
+//           console.log("vous avez gagné !");
+//         }
+//         document.querySelectorAll(".mot-secret > div").forEach((unTiret, indexTiret) => {
+//           for (let indexMotMystere = 0; indexMotMystere < motAleatoire.length; indexMotMystere++) {
+//             if (motAleatoire[indexMotMystere] === unBtn.innerText && indexMotMystere == indexTiret) {
+//               // console.log("arrayMotMystere[indexMotMystere] :", motAleatoire[indexMotMystere]);
+//               // console.log("unBtn.innerText :", unBtn.innerText);
+
+//               unTiret.innerText = motAleatoire[indexMotMystere];
+//             }
+//           }
+//           console.log(unBtn.innerText);
+//         })
+//       } else {
+//         console.log("c'est n'ai pas bon ");
+//         indexImage++;
+//         image.setAttribute("src", `./assests/img/pendu${indexImage}.png`)
+//         nombreDevie--;
+//         console.log("nombre de vies restantes =   " + nombreDevie);
+//         if (nombreDevie === 0) {
+//           console.log("vous avez perdu !");
+//         }
+//       }
+//     }
+//     console.log(saisie);
+//   })
+// })
